adb13bdf3e0784cd3bd00778576750b6f4509a20
Mike Naughton <michael.d.naughton@gmail.com>
2016-07-21 13:38:19 -0500

    Add some other stuff to the error output

--- tasks/dojo.js
+++ tasks/dojo.js
@@ -241,10 +241,10 @@ module.exports = function(grunt) {
     }, function(err, result){
       if(err){
         if(opts.ignoreErrors) {
-          grunt.log.writeln('dojo build error: ' + JSON.stringify(err));
+          grunt.log.writeln('dojo build error: ' + JSON.stringify(err)+err.message+err.stack);
           return done(true);
         } else {
-          grunt.log.error('dojo build error: ' + JSON.stringify(err));
+          grunt.log.error('dojo build error: ' + JSON.stringify(err)+err.message+err.stack);
           return done(false);
         }
       }
