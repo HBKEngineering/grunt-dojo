9dbf1165b9e728a13581d3072fb063bcc492f50d
Mike Naughton <michael.d.naughton@gmail.com>
2016-07-21 12:52:51 -0500

    Stringify the error message

--- tasks/dojo.js
+++ tasks/dojo.js
@@ -241,10 +241,10 @@ module.exports = function(grunt) {
     }, function(err, result){
       if(err){
         if(opts.ignoreErrors) {
-          grunt.log.writeln('dojo build error: ' + err);
+          grunt.log.writeln('dojo build error: ' + JSON.stringify(err));
           return done(true);
         } else {
-          grunt.log.error('dojo build error: ' + err);
+          grunt.log.error('dojo build error: ' + JSON.stringify(err));
           return done(false);
         }
       }
